ff49b5608e6621b7ef2e2c077f861917e913eef8
KEEMU <goldch009@163.com>
2019-07-03 15:06:47 +0800

    move the cube

---
+++ Move.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float speed = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(Vector3.back * Time.deltaTime * speed);
+    }
+}
