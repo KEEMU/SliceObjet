f84b36b5c65afcada46a60abac1e50991de3e959
KEEMU <goldch009@163.com>
2019-07-03 15:05:47 +0800

    add auto find target

--- SliceTest.cs
+++ SliceTest.cs
@@ -1,37 +1,46 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using EzySlice;
 
 public class SliceTest : MonoBehaviour
 {
-    public GameObject target;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.K))
-            SliceObject();
-    }
+
     public SlicedHull SliceObject(GameObject obj, Material crossSectionMaterial = null)
     {
         return obj.Slice(transform.position, transform.up, crossSectionMaterial);
     }
-    public void SliceObject()
+    public void SliceObject(GameObject target)
     {
-        SlicedHull hull = SliceObject(target);
+        SlicedHull hull = SliceObject(target,null);
         if (hull != null)
         {
-            hull.CreateLowerHull(target, null);
-            hull.CreateUpperHull(target, null);
+            GameObject lowerObj = hull.CreateLowerHull(target, null);
+            GameObject upperObj = hull.CreateUpperHull(target, null);
+
+            Rigidbody lowerrd = lowerObj.AddComponent<Rigidbody>();
+            Rigidbody uprd = upperObj.AddComponent<Rigidbody>();
+
+            lowerObj.AddComponent<MeshCollider>().convex = true;
+            upperObj.AddComponent<MeshCollider>().convex = true;
+
+            lowerrd.AddExplosionForce(500f,-transform.right,5f);
+            uprd.AddExplosionForce(500f, transform.right, 5f);
 
             target.SetActive(false);
         }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        SliceObject(other.gameObject);
     }
 
 }
